f66778b18fd4d771ac8a33fc13c06b87b0d86667
Renee Cousins <nonarkitten@gmail.com>
2020-11-24 20:58:10 -0700

    Update pjit.h

--- pjit.h
+++ pjit.h
@@ -10,8 +10,6 @@ typedef union {
     void (*invoke)(void);    
 } caller_t;
 
-
-#ifdef __arm__
 // r0~r12, r14 are available for abuse
 // r0~r1 sratch registers
 // r2~r9 for a0~a7
@@ -43,36 +41,26 @@ ADDRREG(a4,r6) ADDRREG(a5,r7) ADDRREG(a6,r8) ADDRREG(a7,r9)
 #define SET_NZ_W(R) asm volatile("rors %0, %0, #16 @ set nz flags" :: "r"(R))
 #define SET_NZ_L(R) asm volatile("teq %0, #0 @ set nz flags" :: "r"(R))
 
-static inline uint32_t add_uint32_t(uint32_t d, uint32_t s) {
-    uint32_t x = 0;
-    if(__builtin_uaddl_overflow(d, s, &x))
-        asm volatile("" ::: "memory");
-    else
-        asm volatile("" ::: "memory");
-    return x;
-}
+#define EXP_ADD_CVNZ(TYPE,OP,BITS) \
+static inline TYPE add_##TYPE(TYPE d, TYPE s) { \
+    TYPE x = 0; \
+    if(OP(d << BITS, s << BITS, &x)) \
+        asm volatile("" ::: "memory"); \
+    else \
+        asm volatile("" ::: "memory"); \
+    return x >> BITS; }
+  
+EXP_ADD_CVNZ(uint32_t,__builtin_uaddl_overflow,32)
+EXP_ADD_CVNZ(uint16_t,__builtin_uaddl_overflow,16)
+EXP_ADD_CVNZ(uint8_t, __builtin_uaddl_overflow,8)
 
-static inline uint16_t add_uint16_t(uint16_t d, uint16_t s) {
-    uint32_t x = 0;
-    if(__builtin_uaddl_overflow(d << 16, s << 16, &x))
-        asm volatile("" ::: "memory");
-    else
-        asm volatile("" ::: "memory");
-    return (x >> 16);
-}
+EXP_ADD_CVNZ(int32_t,__builtin_saddl_overflow,32)
+EXP_ADD_CVNZ(int16_t,__builtin_saddl_overflow,16)
+EXP_ADD_CVNZ(int8_t, __builtin_saddl_overflow,8)
 
-static inline uint8_t add_uint8_t(uint8_t d, uint8_t s) {
-    uint32_t x = 0;
-    if(__builtin_uaddl_overflow(d << 24, s << 24, &x))
-        asm volatile("" ::: "memory");
-    else
-        asm volatile("" ::: "memory");
-    return (x >> 24);
-}
-
-#define sub_uint32_t(X,Y) add_uint32_t(X,-(Y))
-#define sub_uint16_t(X,Y) add_uint16_t(X,-(Y))
-#define sub_uint8_t(X,Y) add_uint8_t(X,-(Y))
+#define sub_int32_t(X,Y) add_int32_t(X,-(Y))
+#define sub_int16_t(X,Y) add_int16_t(X,-(Y))
+#define sub_int8_t(X,Y)  add_int8_t(X,-(Y))
 
 register uint8_t*  pc_bp asm("r10"); 
 register uint16_t* pc_wp asm("r10"); 
@@ -90,55 +78,6 @@ extern uint8_t arm_sr_ccr[16];
     asm volatile("mrs r0, cpsr"); \
     sr_ccr = (sr_ccr & 0xFF00) | arm_sr_ccr[r0 >> 28]; \
 } while(0)
-
-#else
-/*
-
-Generic Accessor Form
-D(N,W)  N=0..7, W=BWL
-A(N,W)  N=0..7, W= WL
-PC, PC_BPTR, PC_WPTR, PC_LPTR
-SR
-
-*/
-typedef struct { 
-    uint32_t L; 
-    uint16_t W; 
-    uint8_t B; 
-    uint32_t* L_PTR; 
-    uint16_t* W_PTR; 
-    uint8_t* B_PTR; 
-} reg_t;
-
-typedef union {
-    uint16_t sr;
-    struct {
-        int x : 1;
-        int v : 1;
-        int c : 1;
-        int z : 1;
-        int n : 1;
-    };
-} sr_ccr_t;
-
-#define FLAG_X      sr_ccr.z
-#define FLAG_V      sr_ccr.v
-#define FLAG_C      sr_ccr.c
-#define FLAG_Z      sr_ccr.z
-#define FLAG_N      sr_ccr.n
-#define SR          sr_ccr.sr
-inline sr_ccr_t sr_ccr;
-
-#define D(N,W)      (__regs[N].W)
-#define A(N,W)      (__regs[(N)+8].W)
-#define APTR(N,W)   __regs[(N)+8].W##_PTR
-#define PC_BPTR     __regs[16].B_PTR
-#define PC_WPTR     __regs[16].W_PTR
-#define PC_LPTR     __regs[16].L_PTR
-#define PC          __regs[16].L
-inline reg_t __regs[17] = { 0 };
-
-#endif
 
 extern void vec_0000(void);
 extern void vec_0001(void);
