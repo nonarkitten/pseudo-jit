6443ca689f472134593d2af65c97a5c8e26063ba
Renee Cousins <nonarkitten@gmail.com>
2022-11-15 23:14:55 -0700

    Tidy linker; init bss

--- main.c
+++ main.c
@@ -22,6 +22,15 @@ static uint16_t bogomips_ROM[] = {
 };
 
 __attribute__((naked)) void main(void) {
+    asm volatile (                                                                      
+    "           mov r0, #0                      \n"
+    "           ldr r1, =_bss_start             \n"
+    "           ldr r2, =_bss_end               \n"
+    "1:         cmp r1, r2                      \n"
+    "           strlo   r0, [r1], #4            \n"
+    "           blo     1b                      \n"
+    );
+	
 	uint32_t pc = (uint32_t)&bogomips_ROM[4];
 	bogomips_ROM[0] = pc >> 16;
 	bogomips_ROM[1] = pc & 0xFFFF;
